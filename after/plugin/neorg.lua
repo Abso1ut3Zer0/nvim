723c69eb04f3e1f25f4f2ca331b66829447ff7df
Zer0 <mhart626@gmail.com>
2023-10-09 00:16:09 -0400

    Neorg keymaps

---
+++ after/plugin/neorg.lua
@@ -0,0 +1,6 @@
+require('neorg')
+
+vim.keymap.set("n", "<leader>ni", ":Neorg index<CR>")
+vim.keymap.set("n", "<leader>nr", ":Neorg return<CR>")
+vim.keymap.set("n", "<leader>nws", ":Neorg workspace fcshield_tracker<CR>")
+vim.keymap.set("n", "<leader>nwf", ":Neorg workspace fc_tracker<CR>")
