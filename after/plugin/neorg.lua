de6267ea540b75173f6b8cd1f85a1a63933b4f85
Zer0 <mhart626@gmail.com>
2023-11-14 12:55:57 -0500

    Remove export hot key - should use this manually from now on

--- after/plugin/neorg.lua
+++ after/plugin/neorg.lua
@@ -5,7 +5,3 @@ vim.keymap.set("n", "<leader>nr", ":Neorg return<CR>")
 vim.keymap.set("n", "<leader>n1", ":Neorg workspace notes<CR>")
 vim.keymap.set("n", "<leader>n2", ":Neorg workspace fc_tracker<CR>")
 vim.keymap.set("n", "<leader>n3", ":Neorg workspace fcshield_tracker<CR>")
-
--- Export to markdown in project tracker
-vim.keymap.set("n", "<leader>nx2", ":Neorg export to-file ~/fc_tracker/status.md<CR>")
-vim.keymap.set("n", "<leader>nx3", ":Neorg export to-file ~/fcshield_tracker/status.md<CR>")
