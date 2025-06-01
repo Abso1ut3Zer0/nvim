e6580279e212ca2b373d4473364decca4760927e
mhart626 <michael.hart@falconx.io>
2025-06-01 12:36:25 -0400

    fix colors may2025

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,47 +1,49 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+require('kanagawa').setup({
+	compile = false,
+	undercurl = true,
+	commentStyle = { italic = true },
+	functionStyle = { italic = true },
+	keywordStyle = { bold = true },
+	statementStyle = { bold = true },
+	typeStyle = { bold = true },
+	transparent = true,
+	dimInactive = false,
+	terminalColors = false,
+	colors = {
+		palette = {},
+		theme = {
+			wave = {},
+			lotus = {},
+			dragon = {
+				syn = {
+					string = "#87b887",
+					number = "#ff9cb4",
+					constant = "#d8927b",
+					identifier = "#d4bd8d",
+					fun = "#E6ADC2",
+					statement = "#d46767",
+					keyword = "#8F98AE",
+					operator = "#b2a09c",
+					puct = "#b2a09c",
+					comment = "#C3C3C3",
+					preproc = "#d08770",
+					type = "#73A9C0",
+					special1 = "#d8927b",
+					special2 = "#d8927b",
+					special3 = "#d8927b",
+				},
+			},
+			all = { ui = { bg_gutter = "none" } }
+		},
+	},
+	overrides = function(colors)
+		return {}
+	end,
+	theme = "wave",
+	background = {
+		dark = "dragon",
+		light = "lotus"
+	},
+})
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>pp", '"+p')
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-
--- Move to end of current line
-vim.keymap.set("n", "<leader>el", "$")
-
--- Move to beginning of current line
-vim.keymap.set("n", "<leader>bl", "^")
-
--- Remove all default vim-slime maps
-vim.g.slime_no_mappings = 1
-
--- VimTeX remaps
-vim.g.vimtex_view_method = "skim"
-vim.g.vimtex_compiler_progname = "nvr"
-vim.g.vimtex_syntax_enabled = 0
-
--- Vertical Split
-vim.keymap.set("n", "<leader>vs", "<cmd>vsplit<CR>")
-
+vim.cmd('colorscheme kanagawa')
